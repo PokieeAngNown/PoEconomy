76a756762f08b0db84c748d53316b9ef801c71b1
Pokiee <N_2387253457BeiYi@outlook.com>
2024-02-18 20:59:29 +0800

    添加了关于Config.yml中EconomyType的变化 玩家档案做出的调整

--- src/main/java/cn/postudio/poeconomy/EconomyStuff.java
+++ src/main/java/cn/postudio/poeconomy/EconomyStuff.java
@@ -6,24 +6,59 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import javax.sql.PooledConnection;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class EconomyStuff {
 
     public static void initializePlayerEconomy(@NotNull Player player){
         UUID uuid = player.getUniqueId();
         File file = FileFunction.getFile(POFileType.PlayerData, String.valueOf(uuid));
-        List<String> list = getEconomyTypeInConfig();
-        String s;
-        try{
-            for (String string : list) {
-                s = string;
-                FileFunction.writeFile(file, s, 0);
+        FileConfiguration fileConfiguration = FileFunction.getFileCfg(file);
+
+        List<String> economyType = getEconomyTypeInConfig();
+        String s1;
+        if (fileConfiguration.get("Economy") == null){
+            try{
+                for (String string : economyType) {
+                    s1 = string;
+                    FileFunction.writeFile(file, "Economy." + s1, 0);
+                }
+            }catch (IOException ignored){}
+        }else{
+            List<String> l = new ArrayList<>(fileConfiguration.getKeys(true));
+            List<String> playerFileEconomyType = new ArrayList<>();
+            for (String string : l){
+                if (string.contains("Economy.")){
+                    playerFileEconomyType.add(string.replace("Economy.", ""));
+                }
             }
-        }catch (IOException ignored){}
+
+            int a = economyType.size();
+            String s2;
+            for (String s : playerFileEconomyType) {
+                if (!economyType.contains(s)) {
+                    s2 = s;
+                    try {
+                        FileFunction.writeFile(file, "Economy." + s2, null);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            for (int i = 0; i < a; i++) {
+                if (!playerFileEconomyType.contains(economyType.get(i))){
+                    s2 = economyType.get(i);
+                    try {
+                        FileFunction.writeFile(file, "Economy." + s2, 0);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
     }
 
     private static @NotNull List<String> getEconomyTypeInConfig(){
